4897381df1a707dae18aa7b7a7193abd61039b7c
unknown <mohammad999alhos10@gmail.com>
2021-11-26 17:26:23 +0200

    mongodb with nodejs project

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,21 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
-    .then(result => {
-      // console.log(result);
-      console.log('Created Product');
-      res.redirect('/admin/products');
-    })
-    .catch(err => {
-      console.log(err);
-    });
+ const product = new Product(title,price,imageUrl,description,null,req.user._id);
+ product.save()
+ .then((result) => {
+     console.log('Created Product');
+     res.redirect('/admin/products');
+ }).catch((err) => {
+     console.log(err);
+ });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -36,22 +29,17 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findByPk(prodId)
-    .then(products => {
-      const product = products[0];
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
-    })
-    .catch(err => console.log(err));
+
+  Product.findById(prodId)
+  .then(product=>{
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -60,14 +48,8 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+  updateProduct = new Product(updatedTitle, updatedPrice,updatedImageUrl,updatedDesc,prodId);
+ return updateProduct.save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -76,8 +58,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -90,10 +71,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy();
-    })
+  Product.deleteById(prodId)
     .then(result => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
--- util/database.js
+++ util/database.js
@@ -1,8 +1,22 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'ASmas#10160lkF', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+const mongodb  = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb://localhost:27017"
+let _db;
+// you can replace test with any database name that you want
+const mongoConnect =(cb) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      _db = client.db();
+      cb()
+      console.log('Connected to MongoDb');
+    }).catch((err) => {
+      console.log(err);
+    });
+}
+//after your server started you can use getDb to access mongo Database
+const getDb = () => {
+  if (_db) return _db;
+  throw 'No database found';
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
